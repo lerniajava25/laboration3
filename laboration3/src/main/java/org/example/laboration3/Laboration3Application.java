b3907ca92807ea6f5ae4bba135abd68f08e3fcad
maju0029 <maju0029@gmail.com>
2026-09-07 10:30:13 +0200

    initial commit springBoot

---
+++ laboration3/src/main/java/org/example/laboration3/Laboration3Application.java
@@ -0,0 +1,13 @@
+package org.example.laboration3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboration3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboration3Application.class, args);
+    }
+
+}
---
+++ laboration3/src/test/java/org/example/laboration3/Laboration3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.laboration3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboration3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
